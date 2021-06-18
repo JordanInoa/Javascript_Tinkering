6d8cbee214d254557d88850804bc2f1e17fe61c3
Jordan Inoa <jordan.inoa@gmail.com>
2021-06-17 22:53:26 -0400

    javascript tinkering

---
+++ Inoa_Jordan_JS_Tinkering.js
@@ -0,0 +1,9 @@
+function displayIfchildisabletoridetherollercoaster() {
+    var childheight = 66;
+    if (childheight > 52) {
+        console.log("Get on that ride kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo, maybe next year.");
+    }
+}
